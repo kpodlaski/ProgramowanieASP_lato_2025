8f39e7fefcf55c77453518fcdb8b2e8f9c592d2c
kpodlaski <podlaski@uni.lodz.pl>
2025-03-27 09:56:19 +0100

    27.03.2025

---
+++ SpecC/SimpleThreads/EvenCounter.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleThreads
+{
+    internal class Tester
+    {
+        EvenCounter ec;
+        public Tester(EvenCounter ec) { this.ec = ec; }
+        public void testParity()
+        {
+            //Console.WriteLine("Rozpoczynam testowanie!!");
+            int t = 0;
+            while (true)
+            {
+                lock (ec)
+                {
+                    t = ec.counter;
+                }
+                if (t % 2 == 1)
+                {
+                    Console.WriteLine("ERRROR " + ec.counter + "  t:" + t);
+                    Environment.Exit(12);
+                }
+            }
+
+        }
+    }
+
+    internal class EvenCounter
+    {
+        public int  counter = 0;
+
+        public void count()
+        {
+            while (true)
+            {
+                lock (this)
+                {
+                    counter += 1;
+                    counter += 1;
+                }
+            }
+        }
+
+        
+
+        public static void Main()
+        {
+            EvenCounter ec = new EvenCounter();
+            Tester tester = new Tester(ec);
+            Thread count = new(new ThreadStart(ec.count));
+            Thread test = new(new ThreadStart(tester.testParity));
+            count.Start();
+            test.Start();
+            
+        }
+    }
+}
---
+++ SpecC/SimpleThreads/SimpleThread.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleThreads
+{
+    internal class SimpleThread
+    {
+        private static int i  = 0;
+        // The method that will be called when the thread is started.
+        public void InstanceMethod()
+        {
+            int c = 0;
+            for (i = 0; i < 3000; i++)
+            {
+                c += 1;
+                Console.WriteLine("A" + c);
+            }
+            Console.WriteLine("A" + c);
+        }
+
+        public static void StaticMethod()
+        {
+            int c = 0;
+            for (i = 0; i < 3000; i++)
+            {
+                c += 1;
+                Console.WriteLine("B" + c);
+            }
+            Console.WriteLine("B" + c);
+        }
+
+        public static void Main()
+        {
+            SimpleThread serverObject = new SimpleThread();
+
+            // Create the thread object, passing in the
+            // serverObject.InstanceMethod method using a
+            // ThreadStart delegate.
+            Thread InstanceCaller = new(new ThreadStart(serverObject.InstanceMethod));
+
+            // Start the thread.
+            InstanceCaller.Start();
+
+            Console.WriteLine("The Main() thread calls this after "
+                + "starting the new InstanceCaller thread.");
+
+            // Create the thread object, passing in the
+            // serverObject.StaticMethod method using a
+            // ThreadStart delegate.
+            Thread StaticCaller = new(new ThreadStart(SimpleThread.StaticMethod));
+
+            // Start the thread.
+            StaticCaller.Start();
+
+            Console.WriteLine("The Main() thread calls this after "
+                + "starting the new StaticCaller thread.");
+        }
+    }
+}
